7ecc84decb6b813009fad24174eb344f440577eb
Andre <andre.p23@hotmail.com>
2017-02-22 01:04:39 -0800

    Add: configuration for env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+require('../config/config');
+
+console.log(`env ******* ${env}`);
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +12,7 @@ const {Todo} =require('./models/todo');
 const {User} =require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
