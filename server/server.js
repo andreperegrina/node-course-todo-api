b7f2aa1f07bbb3f2cae43251a6a959aa28565e3d
Andre <andre.p23@hotmail.com>
2017-02-21 23:46:27 -0800

    Add: DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} =require('./models/todo');
 const {User} =require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +47,23 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+
+        res.send({todo});
+    }).catch((e) => {
+        return res.status(400).send()
+    });
 });
 
 module.exports = {app};
